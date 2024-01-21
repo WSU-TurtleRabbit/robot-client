7202046e031804d47fb518f031235b3a1ce4e3fe
Jason N <7614175+jsnng@users.noreply.github.com>
2024-01-21 20:51:56 +1100

    added rpi setup script

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #! /usr/bin/env bash
 
+sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED
+curl https://pyenv.run/ | bash
+
+echo 'export PATH=$HOME/.pyenv/bin:$PATH' >> ~./bashrc
+echo 'eval "$(pyenv init --path")' >> ~./bashrc 
+echo 'eval "$(pyenv virtualenv-init -")' >> ~./bashrc 
