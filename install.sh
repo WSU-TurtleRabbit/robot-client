03a8bb7f962cf4e6c1f8a182e234991aeda70ee2
Emmama222 <20544166@student.westernsydney.edu.au>
2024-01-21 21:29:57 +1100

    added

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ pyenv update
 
 pyenv install 3.7.12
 pyenv global 3.7.12
+
 pip install --upgrade pip
 sudo pip3 install moteus
 pip3 install moteus-pi3hat
@@ -21,3 +22,16 @@ pip3 install moteus-pi3hat
 sudo apt install python3-pyside2* python3-serial python3-can python3-matplotlib python3-qtconsole
 sudo pip3 install asyncqt importlib_metadata pyelftools
 sudo pip3 install --no-deps moteus moteus_gui
+
+python3 -m moteus.moteus_tool --pi3hat-cfg '1=1,2=2;3=3,4=4' -t 1,2,3,4
+
+curl https://github.com/mjbots/moteus/releases/download/0.1-20240106/20240106-moteus-1ba9e683e207d792060b45ea435dd6905589a7d4.elf 
+for id in $$(seq 1 4);
+do 
+    python3 -m moteus.moteus_tool --target $id --flash '20240106-moteus-1f4a19554a2b99578cdf566ec5172ee7ddc8304f.elf'   
+    python3 -m moteus.moteus_tool --target $id --calibrate  
+    
+    python3 -m moteus.moteus_tool --target $id --console conf set servopos.position_min nan  
+    python3 -m moteus.moteus_tool --target $id --console conf set servopos.position_max nan  
+    python3 -m moteus.moteus_tool --target $id --console conf write 
+done
