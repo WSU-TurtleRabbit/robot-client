15e0ee24bfe3aa362c4fe2bb7e19bec938908c66
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-22 14:24:00 +1100

    organised setup.sh

---
+++ README.md
@@ -0,0 +1,9 @@
+# Installation
+```bash
+
+git clone https://github.com/WSU-TurtleRabbit/robot-client.git 
+cd robot-client
+chmod 755 setup.sh
+./setup.sh
+pip3 install --editable .
+```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
+PYVERSION=3.7.12
+
+[ "$EUID" -eq 0 ] || sudo "$0" "$@"
 
 sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED
 curl https://pyenv.run/ | bash
@@ -14,11 +13,17 @@ echo 'eval "$(pyenv virtualenv-init -")' >> ~./bashrc
 
 exec $SHELL
 
-sudo apt-get install --yes libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev lzma lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev wget curl make build-essential openssl 
+sudo apt-get install --yes libssl-dev zlib1g-dev libbz2-dev \
+                         libreadline-dev libsqlite3-dev llvm \
+                         libncurses5-dev libncursesw5-dev \
+                         xz-utils tk-dev libgdbm-dev lzma \
+                         lzma-dev tcl-dev libxml2-dev libxmlsec1-dev \
+                         libffi-dev liblzma-dev wget curl make \
+                         build-essential openssl
 pyenv update
 
-pyenv install 3.7.12
-pyenv global 3.7.12
+pyenv install $PYVERSION
+pyenv global $PYVERSION
 
 pip install --upgrade pip
 sudo pip3 install moteus
@@ -31,7 +36,7 @@ sudo pip3 install --no-deps moteus moteus_gui
 python3 -m moteus.moteus_tool --pi3hat-cfg '1=1,2=2;3=3,4=4' -t 1,2,3,4
 
 curl https://github.com/mjbots/moteus/releases/download/0.1-20240106/20240106-moteus-1ba9e683e207d792060b45ea435dd6905589a7d4.elf 
-for id in $$(seq 1 4);
+for id in $(seq 1 4);
 do 
     python3 -m moteus.moteus_tool --target $id --flash '20240106-moteus-1f4a19554a2b99578cdf566ec5172ee7ddc8304f.elf'   
     python3 -m moteus.moteus_tool --target $id --calibrate  
@@ -40,5 +45,3 @@ do
     python3 -m moteus.moteus_tool --target $id --console conf set servopos.position_max nan  
     python3 -m moteus.moteus_tool --target $id --console conf write 
 done
-
-pip3 install --editable .
