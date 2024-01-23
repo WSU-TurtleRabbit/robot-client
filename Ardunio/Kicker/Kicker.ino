24f0dce5fc0b567194318215f50806233121d204
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-24 08:30:28 +1100

    added comments to kicker sample code

--- Ardunio/Kicker/Kicker.ino
+++ Ardunio/Kicker/Kicker.ino
@@ -2,21 +2,28 @@ const int outputPin = 8;
 void pulsePin(int outputPin, int pulseTime);
 
 void setup() {
-  Serial.begin(19200);
-  pinMode(outputPin, OUTPUT);
+  Serial.begin(19200); // set baudrate to 19200
+  pinMode(outputPin, OUTPUT); // set outputPin mode to output
 }
 
 void loop() {
   char data;
-  if(Serial.available() > 0) {
+  if(Serial.available() > 0) { 
     data = Serial.read();
-    if(data == 'K') {
-      pulsePin(outputPin, 200000);
+    if(data == 'K') { // if the data recv'ed is 'K' (note: this is case sensitive)
+      pulsePin(outputPin, 200000); 
     }
   }
 }
 
 void pulsePin(int outputPin, int pulseTime){
+  /*
+  * pulsePin(int outputPin, int pulseTime)
+  * sends a pulse (active high) into `outputPin` when called.
+  * @args
+  * outputPin (int): pin to send the pulse to
+  * pulseTime (int): length of pulse in microseconds
+  */
   digitalWrite(outputPin, HIGH);
   delayMicroseconds(pulseTime);
   digitalWrite(outputPin,LOW);
--- Ardunio/sample.py
+++ Ardunio/sample.py
@@ -1,7 +1,9 @@
 import serial
 
 if __name__ == '__main__':
-    with serial.Serial('/dev/cu.usbmodem101', 19200) as s:
+    # access serial device at /dev/cu.usbmodem101 using baudrate of 192000
+    with serial.Serial('/dev/cu.usbmodem101', 19200) as s: 
+        # write a 'K'
         s.write(b'K')
+        # release the serial device to avoid a 'device is busy' error
         s.close()
-            
