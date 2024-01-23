3e53e8e1057982d656c43e5792914fa5632278c7
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-23 21:30:21 +1100

    update and upgrade

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 
-# [ "$EUID" -eq 0 ] || sudo "$0" "$@"
+[ "$EUID" -eq 0 ] || sudo "$0" "$@"
 
-sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED
+sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED*
 curl https://pyenv.run/ | bash
+
+sudo apt-get update
+sudo apt-get upgrade --yes 
+
+sudo apt-get install --yes libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev lzma lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev wget curl make build-essential openssl
 
 touch ~/.bashrc
 
@@ -11,28 +16,21 @@ echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc 
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc 
 
-exec $SHELL
-# source ~/.bashrc
+# exec $SHELL
+source ~/.bashrc
 
-sudo apt-get install --yes libssl-dev zlib1g-dev libbz2-dev \
-                         libreadline-dev libsqlite3-dev llvm \
-                         libncurses5-dev libncursesw5-dev \
-                         xz-utils tk-dev libgdbm-dev lzma \
-                         lzma-dev tcl-dev libxml2-dev libxmlsec1-dev \
-                         libffi-dev liblzma-dev wget curl make \
-                         build-essential openssl
 pyenv update
 
 pyenv install 3.7.12
 pyenv global 3.7.12
 
-pip install --upgrade pip
+pip3 install --upgrade pip3
 sudo pip3 install moteus
 pip3 install moteus-pi3hat
 
-sudo apt install --yes python3-pyside2* python3-serial python3-can python3-matplotlib python3-qtconsole
+sudo apt-get install --yes python3-pyside2* python3-serial python3-can python3-matplotlib python3-qtconsole
 sudo pip3 install asyncqt importlib_metadata pyelftools
-sudo pip3 install --no-deps moteus moteus_gui
+sudo pip3 install --no-deps moteus_gui
 
 python3 -m moteus.moteus_tool --pi3hat-cfg '1=1,2=2;3=3,4=4' -t 1,2,3,4
 
