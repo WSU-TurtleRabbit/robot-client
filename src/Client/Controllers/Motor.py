d94d928f63faad0e76fb00851fe0805d40e7681b
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-26 21:07:51 +1100

    added docstrings and comments

--- src/Client/Controllers/Motor.py
+++ src/Client/Controllers/Motor.py
@@ -7,10 +7,19 @@ import moteus
 import moteus_pi3hat
 
 class Motor():
-    def __init__(self, timeout=.02):
+    def __init__(self):
+        """_summary_
+            initiate the motor controller with Moteus and Moteus pi3hat
+        Params : 
+            timeout(float) : standard values for await
+            servo_bus_map (dict) : maps pi3hat-fdcan id to moteus boards
+            transport(pi3hat-router): applys the map onto the pi3hat and initialise
+            servos(map) : establish connection of moteus boards and pi3hat
+        """
 
-        self.timeout = timeout
-        self.servo_bus_map = {
+        self.timeout = 0.02
+    
+        self.servo_bus_map = { 
                     1: [1],
                     2: [2],
                     3: [3],
@@ -28,10 +37,22 @@ class Motor():
         self.set_b()
         self.set_d()
         self.set_r()
-        print(self)
+        print("Motor Controller initialised") #END
 
 
     async def run(self, action):
+        """_summary_
+        runs the action (moving) applying to wheels
+
+        Args:
+            action (Action): from action script import action string (vx,vy,omega)
+        Params:
+            cmd (dict): complies the motor make position command into a dictionary
+            end (timer): sets timer for continuous runtime.
+            results(complier) : runs the compiler (cmd) applies to all moteus boards via self.transport
+        """
+
+        ### Extracts from the ACTION sent
         vx = getattr(action, 'vx')
         vy = getattr(action, 'vy')
         omega = getattr(action, 'omega')
@@ -41,17 +62,38 @@ class Motor():
                 position=math.nan,
                 velocity=velocity,
                 query=True
-            ) for id, velocity in enumerate(self.calculate(vx, vy, omega))
+            ) for id, velocity in enumerate(self.calculate(vx, vy, omega)) #calculate the velocity and send them back here
+            ## *This is a backwards for loop*
         ]
         
+        #initialise timer with : 5
         end = time.time() + 5
+        # while the time is still in range
         while time.time() < end :
+            # loop velocity
             results = await self.transport.cycle(cmd)
+            # print debug results
             print(results)
- 
+        #stops after the timer has ran out
         await self.transport.cycle(x.make_stop() for x in self.servos.values())
 
+
     def calculate(self, omega, vx, vy):
+        """_summary_
+
+        Args:
+            omega (float): angle velocity (rad/s)
+            vx (float): velocity in x direction (cm/s)
+            vy (float): velocity in y direction (cm/s)
+
+        Params: 
+            vb (matrix (1,3)): compiles the 3 velocity into an array
+            H (matrix(4,3)): applies the Omniwheel veloicty matrix
+            H.T: transpose H matrix into (3,4)
+
+        Returns:
+            u (array): returns all calculated wheel velocity
+        """
         vb = np.array([omega, vx, vy])
         vb = np.expand_dims(vb, axis=1)
         H = np.array([[-self.d1, -self.d2, -self.d3, -self.d4],
@@ -63,12 +105,31 @@ class Motor():
         return u
     
     def set_b(self, b1=120, b2=45, b3=-45, b4=-120):
+        """_summary_
+
+        Args:
+            b1 (int, degrees): wheel degree to centre. Defaults to 120.
+            b2 (int, degrees): wheel degree to centre. Defaults to 45.
+            b3 (int, degrees): wheel degree to centre. Defaults to -45.
+            b4 (int, degrees): wheel degree to centre. Defaults to -120.
+            *THESE WILL BE CHANGED INTO RADIANS AFTERWARDS*
+        """
         self.b1 = np.radians(b1)
         self.b2 = np.radians(b2)
         self.b3 = np.radians(b3)
         self.b4 = np.radians(b4)
 
     def set_d(self, d1=(61,35), d2=(50,50), d3=(50,50), d4=(61,32)):
+        """_summary_
+            Sets the distance of each wheels from the centre
+
+        Args: (mm)
+            d1 (tuple, coordinates): . Defaults to (61,35).
+            d2 (tuple, coordinates): . Defaults to (50,50).
+            d3 (tuple, coordinates): . Defaults to (50,50).
+            d4 (tuple, coordinates): . Defaults to (61,32).
+            
+        """
         self.d1 = np.sqrt((d1[0])^2+(d1[1])^2)/10
         self.d2 = np.sqrt(d2[0]^2+d2[1]^2)/10
         self.d3 = np.sqrt(d3[0]^2+d3[1]^2)/10
@@ -76,6 +137,11 @@ class Motor():
         print(d1,d2,d3,d4)
 
     def set_r(self, r=33.5):
+        """_summary_
+
+        Args:
+            r (float, radius(mm)): radius of wheels. Defaults to 33.5.
+        """
         self.r = r/10
 
     @staticmethod
