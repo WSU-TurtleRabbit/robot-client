c2dbdd2153464923af637f6dbaff084c7b1de38e
Emmama222 <20544166@studnet.westernsydney.edu.au>
2024-01-27 16:01:17 +1000

    updateed Action with encode and decode. (compatible with UDP scripts)

--- src/Client/Shared/Action.py
+++ src/Client/Shared/Action.py
@@ -1,20 +1,46 @@
 
 class Action:
     def __init__(self, vx: float, vy: float, omega: float, kick: bool, dribble: float):
+        """_summary_
+            Object for initialise action commands, encode / decode strings for UDP transportation.
+        Args:
+            vx (float): wanted velocity for x direction
+            vy (float): wanted velocity for y direction
+            omega (float): wanted angular velocity (radians)
+            kick (bool): wanted kicker to kick (Yes/No)
+            dribble (float): dribbling speed ? 
+        """
         self.vx = vx
         self.vy = vy
         self.omega = omega
         self.kick = kick
         self.dribble = dribble
-
-    @classmethod
-    def decode(cls): 
-        pass
     
-    @classmethod
-    def encode(cls):
-        pass
+    def encode(self):
+        """_summary_
+            Turns everything within the action class into a string
+        Returns:
+            message(string): string for send message to send
+        """
+        self.msg = f"{self.vx} {self.vy} {self.omega} {self.kick} {self.dribble}"
+        return self.msg
 
-    def __repr__(self):
+    @staticmethod
+    def decode(msg):
+        """_summary_
+            decodes the message receives upon UDP transportation.
+        Args:
+            msg (string): message received upon UDP
+
+        Returns:
+            Action (Object): new Action object Model for easier attribute access
+        """
+        vx, vy, omega, kick, dribble = msg.decode().split(" ")
+        args = list(float(vx),float(vy),float(omega),bool(kick),float(dribble))
+        return Action(*args)
+        
+    
+    
+    def __repr__(self): #debug msg
         return f"Action: (vx: {self.vx}, vy: {self.vy}, theta: {self.omega}, kick: {self.kick}, dribble: {self.dribble})"
     
