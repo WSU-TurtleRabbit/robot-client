e1cc69014ebada4a1855ba687cd44de4e22a2458
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-28 19:00:35 +1100

    calculate() method in Motor reworked

--- src/Client/Controllers/Motor.py
+++ src/Client/Controllers/Motor.py
@@ -3,6 +3,7 @@ import math
 
 import numpy as np
 import time
+import functools
 
 import moteus
 import moteus_pi3hat
@@ -38,8 +39,8 @@ class Motor(BaseController):
                 id: moteus.Controller(id=id, transport=self.transport)
                 for id in self.servo_bus_map.keys()
             }
-        self.set_b() # sets wheel degrees
-        self.set_d() # sets distance to centre from each wheel
+        # self.set_b() # sets wheel degrees
+        self.set_d_and_b() # sets distance to centre from each wheel
         self.set_r() # sets radius of the wheel
         print("Motor Controller initialised") #END
 
@@ -63,6 +64,8 @@ class Motor(BaseController):
         vy = getattr(action, 'vy')
         omega = getattr(action, 'omega')
 
+        print(self.calculate(vx, vy, omega))
+
         cmd = [
             self.servos[id+1].make_position(
                 position=math.nan,
@@ -73,18 +76,18 @@ class Motor(BaseController):
         ]
         
         #initialise timer with : 5
-        end = time.time() + 5
+        end = time.time() + 0.05
         # while the time is still in range
         while time.time() < end :
             # loop velocity
             results = await self.transport.cycle(cmd)
             # print debug results
-            print(results)
+            # print(results)
         #stops after the timer has ran out
         await self.transport.cycle(x.make_stop() for x in self.servos.values())
 
 
-    def calculate(self, omega, vx, vy):
+    def calculate(self, vw, vx, vy):
         """_summary_
             calculates omniwheels' velocities using args: vx, vy and omega
             applying the omniwheel equation from:
@@ -93,7 +96,7 @@ class Motor(BaseController):
             13.2.1
 
         Args:
-            omega (float): angle velocity (rad/s)
+            w (float): angle velocity (rad/s)
             vx (float): velocity in x direction (cm/s)
             vy (float): velocity in y direction (cm/s)
 
@@ -105,32 +108,46 @@ class Motor(BaseController):
         Returns:
             w (array): returns all calculated wheel velocity
         """
-        vb = np.array([omega, vx, vy])
-        vb = np.expand_dims(vb, axis=1)
-        H = np.array([[-self.d1, -self.d2, -self.d3, -self.d4],
-        [np.cos(self.b1), np.cos(self.b2), -np.cos(self.b3), -np.cos(self.b4)],
-        [np.sin(self.b1), -np.sin(self.b2), -np.sin(self.b3), np.sin(self.b4)],
+        uv =  np.array([
+            (1. / self.r) * ((self.d1 * vw) - (vx * np.sin(self.b1)) + (vy * np.cos(self.b1))),
+            (1. / self.r) * ((self.d2 * vw) - (vx * np.sin(self.b2)) + (vy * np.cos(self.b2))),
+            (1. / self.r) * ((self.d3 * vw) - (vx * np.sin(self.b3)) + (vy * np.cos(self.b3))),
+            (1. / self.r) * ((self.d4 * vw) - (vx * np.sin(self.b4)) + (vy * np.cos(self.b4)))
         ])
 
-        w = (H.T@vb)/self.r
-        return w
-    
-    def set_b(self, b1=120, b2=45, b3=-45, b4=-120):
-        """_summary_
+        if vy >= 0 and vx <= 0:
+            uv = uv * np.array([1., 1., -1., -1.]).T
+        
+        elif vy >= 0 and vx >= 0:
+            uv = uv * np.array([-1., 1., 1., -1.]).T
 
-        Args:
-            b1 (int, degrees): wheel degree to centre. Defaults to 120.
-            b2 (int, degrees): wheel degree to centre. Defaults to 45.
-            b3 (int, degrees): wheel degree to centre. Defaults to -45.
-            b4 (int, degrees): wheel degree to centre. Defaults to -120.
-            *THESE WILL BE CHANGED INTO RADIANS AFTERWARDS*
-        """
-        self.b1 = np.radians(b1)
-        self.b2 = np.radians(b2)
-        self.b3 = np.radians(b3)
-        self.b4 = np.radians(b4)
+        elif vy <= 0 and vx <= 0:
+            uv = uv * np.array([-1., -1., 1., 1.]).T
 
-    def set_d(self, d1=(61,35), d2=(50,50), d3=(50,50), d4=(61,32)):
+        elif vy <= 0 and vx <= 0:
+            uv = uv * np.array([1., -1., -1., 1.]).T
+
+        print(uv)
+        return uv
+
+        # normalisation = np.sqrt(np.sum([uv[x]*uv[x] for x in range(uv)]))
+        
+    # def set_b(self, b1=120, b2=45, b3=-45, b4=-120):
+    #     """_summary_
+
+    #     Args:
+    #         b1 (int, degrees): wheel degree to centre. Defaults to 120.
+    #         b2 (int, degrees): wheel degree to centre. Defaults to 45.
+    #         b3 (int, degrees): wheel degree to centre. Defaults to -45.
+    #         b4 (int, degrees): wheel degree to centre. Defaults to -120.
+    #         *THESE WILL BE CHANGED INTO RADIANS AFTERWARDS*
+    #     """
+    #     self.b1 = np.radians(b1)
+    #     self.b2 = np.radians(b2)
+    #     self.b3 = np.radians(b3)
+    #     self.b4 = np.radians(b4)
+
+    def set_d_and_b(self, d1=(61,35), d2=(50,-50), d3=(-50,-50), d4=(-61,32)):
         """_summary_
             Sets the distance of each wheels from the centre using Pythagorus Theorum.
 
@@ -141,11 +158,25 @@ class Motor(BaseController):
             d4 (tuple, coordinates): . Defaults to (61,32).
             
         """
-        self.d1 = np.sqrt((d1[0])^2+(d1[1])^2)/self.u
-        self.d2 = np.sqrt(d2[0]^2+d2[1]^2)/self.u
-        self.d3 = np.sqrt(d3[0]^2+d3[1]^2)/self.u
-        self.d4 = np.sqrt(d4[0]^2+d4[1]^2)/self.u
-        print(d1,d2,d3,d4)
+
+        # d1 = tuple([np.abs(x) for x in d1])
+        # d2 = tuple([np.abs(x) for x in d2])
+        # d3 = tuple([np.abs(x) for x in d3])
+        # d4 = tuple([np.abs(x) for x in d4])
+
+        self.d1 = np.sqrt(d1[0]**2+d1[1]**2)/self.u
+        self.d2 = np.sqrt(d2[0]**2+d2[1]**2)/self.u
+        self.d3 = np.sqrt(d3[0]**2+d3[1]**2)/self.u
+        self.d4 = np.sqrt(d4[0]**2+d4[1]**2)/self.u
+
+        print(self.d1, self.d2, self.d3, self.d4)
+
+        self.b1 = np.radians(np.tan(d1[0]/d1[1]))
+        self.b2 = np.radians(np.tan(d2[0]/d2[1]))
+        self.b3 = np.radians(np.tan(d3[0]/d3[1]))
+        self.b4 = np.radians(np.tan(d4[0]/d4[1]))
+
+        print(self.b1, self.b2, self.b3, self.b4)
 
     def set_r(self, r=33.5):
         """_summary_
