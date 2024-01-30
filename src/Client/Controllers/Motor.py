cf490cc5477b72eccdb382420237596df7b1b4c0
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-30 19:43:56 +1100

    motor controller calculate() updated

--- src/Client/Controllers/Motor.py
+++ src/Client/Controllers/Motor.py
@@ -3,7 +3,6 @@ import math
 
 import numpy as np
 import time
-import functools
 
 import moteus
 import moteus_pi3hat
@@ -21,8 +20,8 @@ class Motor(BaseController):
         """
 
         super().__init__()
-        self.timeout = 0.02
-        self.u = 1
+
+        self.u = 1.
     
         self.servo_bus_map = { 
                     1: [1],
@@ -39,8 +38,8 @@ class Motor(BaseController):
                 id: moteus.Controller(id=id, transport=self.transport)
                 for id in self.servo_bus_map.keys()
             }
-        # self.set_b() # sets wheel degrees 
-        self.set_d_and_b() # sets distance to centre from each wheel
+        self.set_b() # sets wheel degrees 
+        self.set_d() # sets distance to centre from each wheel
         self.set_r() # sets radius of the wheel
         print("Motor Controller initialised") #END
 
@@ -62,29 +61,79 @@ class Motor(BaseController):
         ### Extracts from the ACTION sent
         vx = getattr(action, 'vx')
         vy = getattr(action, 'vy')
-        omega = getattr(action, 'omega')
+        vw = getattr(action, 'vw')
 
-        print(self.calculate(vx, vy, omega))
+        print(self.calculate(vx, vy, vw))
 
         cmd = [
             self.servos[id+1].make_position(
                 position=math.nan,
                 velocity=velocity,
                 query=True
-            ) for id, velocity in enumerate(self.calculate(vx, vy, omega)) #calculate the velocity and send them back here
+            ) for id, velocity in enumerate(self.calculate(vw, vx, vy)) #calculate the velocity and send them back here
             ## *This is a backwards for loop*
         ]
-        
+
+        print(cmd)
+
         #initialise timer with : 5
-        end = time.time() + 0.05
+        end = time.time() + 0.5
         # while the time is still in range
         while time.time() < end :
             # loop velocity
             results = await self.transport.cycle(cmd)
             # print debug results
             # print(results)
+            # pass
         #stops after the timer has ran out
         await self.transport.cycle(x.make_stop() for x in self.servos.values())
+
+    def _calculate(self, vw, vx, vy):
+        raise DeprecationWarning("_calculate() has been deprecated. use calculate()")
+        """_summary_
+            calculates omniwheels' velocities using args: vx, vy and omega
+            applying the omniwheel equation from:
+            
+            "Modern Robotics: Mechanics, Planning & Control"
+            13.2.1
+
+        Args:
+            omega (float): angle velocity (rad/s)
+            vx (float): velocity in x direction (cm/s)
+            vy (float): velocity in y direction (cm/s)
+
+        Params: 
+            vb (matrix (1,3)): compiles the 3 velocity into an array
+            H (matrix(4,3)): applies the Omniwheel veloicty matrix
+            H.T: transpose H matrix into (3,4)
+
+        Returns:
+            w (array): returns all calculated wheel velocity
+        """
+        vb = np.array([vw, vx, vy])
+        vb = np.expand_dims(vb, axis=1)
+        H = np.array([[-self.d1, -self.d2, -self.d3, -self.d4],
+        [np.cos(self.b1), np.cos(self.b2), -np.cos(self.b3), -np.cos(self.b4)],
+        [np.sin(self.b1), -np.sin(self.b2), -np.sin(self.b3), np.sin(self.b4)],
+        ])
+
+        w = (H.T@vb)/self.r
+        return w
+    
+
+    def _calculate(self, vw, vx, vy):
+        raise DeprecationWarning("_calculate() has been deprecated, use calculate()")
+        self.set_r(self.r/1000)
+        uv =  np.array([
+            (1. / self.r) * ((np.cos(self.b1)*(-0.036875 * vw + vx)+np.sin(self.b1) * (0.063869* vw + vy))),
+            (1. / self.r) * ((np.cos(self.b2)*(0.052149 * vw + vx)+np.sin(self.b2) * (0.052149* vw + vy))),
+            (1. / self.r) * ((np.cos(self.b3)*(0.052149 * vw + vx)+np.sin(self.b3) * (0.052149* vw + vy))),
+            (1. / self.r) * ((np.cos(self.b4)*(-0.036875 * vw + vx)+np.sin(self.b4) * (-0.063869* vw + vy)))
+        ])
+
+        # uv = np.array([x/(2*math.pi) for x in uv])
+        print(f"{uv=}")
+        return uv
 
     def calculate(self, vw, vx, vy):
         """_summary_
@@ -107,46 +156,40 @@ class Motor(BaseController):
         Returns:
             w (array): returns all calculated wheel velocity
         """
+
+        # self.u = 1000
+        # self.set_d()
+        # self.set_r()
+
         uv =  np.array([
             (1. / self.r) * ((self.d1 * vw) - (vx * np.sin(self.b1)) + (vy * np.cos(self.b1))),
             (1. / self.r) * ((self.d2 * vw) - (vx * np.sin(self.b2)) + (vy * np.cos(self.b2))),
             (1. / self.r) * ((self.d3 * vw) - (vx * np.sin(self.b3)) + (vy * np.cos(self.b3))),
             (1. / self.r) * ((self.d4 * vw) - (vx * np.sin(self.b4)) + (vy * np.cos(self.b4)))
         ])
-
-        if vy >= 0 and vx <= 0:
-            uv = uv * np.array([1., 1., -1., -1.]).T
-        
-        elif vy >= 0 and vx >= 0:
-            uv = uv * np.array([-1., 1., 1., -1.]).T
-
-        elif vy <= 0 and vx <= 0:
-            uv = uv * np.array([-1., -1., 1., 1.]).T
-
-        elif vy <= 0 and vx <= 0:
-            uv = uv * np.array([1., -1., -1., 1.]).T
-
-        print(uv)
+        print(f"{__name__}.calculate({vw=}, {vx=}, {vy=}) = {uv=}")
+        uv = np.multiply(uv, 1/2*np.pi)
+        print(f"{__name__}.calculate({vw=}, {vx=}, {vy=}) = {uv=}")
         return uv
-
-        # normalisation = np.sqrt(np.sum([uv[x]*uv[x] for x in range(uv)]))
         
-    # def set_b(self, b1=120, b2=45, b3=-45, b4=-120):
-    #     """_summary_
+    def set_b(self, b1=120, b2=45, b3=-45, b4=-120):
+        """_summary_
 
-    #     Args:
-    #         b1 (int, degrees): wheel degree to centre. Defaults to 120.
-    #         b2 (int, degrees): wheel degree to centre. Defaults to 45.
-    #         b3 (int, degrees): wheel degree to centre. Defaults to -45.
-    #         b4 (int, degrees): wheel degree to centre. Defaults to -120.
-    #         *THESE WILL BE CHANGED INTO RADIANS AFTERWARDS*
-    #     """
-    #     self.b1 = np.radians(b1)
-    #     self.b2 = np.radians(b2)
-    #     self.b3 = np.radians(b3)
-    #     self.b4 = np.radians(b4)
+        Args:
+            b1 (int, degrees): wheel degree to centre. Defaults to 120.
+            b2 (int, degrees): wheel degree to centre. Defaults to 45.
+            b3 (int, degrees): wheel degree to centre. Defaults to -45.
+            b4 (int, degrees): wheel degree to centre. Defaults to -120.
+            *THESE WILL BE CHANGED INTO RADIANS AFTERWARDS*
+        """
+        self.b1 = np.radians(b1)
+        self.b2 = np.radians(b2)
+        self.b3 = np.radians(b3)
+        self.b4 = np.radians(b4)
 
-    def set_d_and_b(self, d1=(63.869,36.875), d2=(52.149,-52.149), d3=(-52.149,-52.149), d4=(-63.869,36.875)):
+        print(f"{self.b1=}, {self.b2=}, {self.b3=}, {self.b4=}")
+
+    def set_d(self, d1=(63.869,36.875), d2=(52.149,-52.149), d3=(-52.149,-52.149), d4=(-63.869,36.875)):
         """_summary_
             Sets the distance of each wheels from the centre using Pythagorus Theorum.
 
@@ -158,24 +201,13 @@ class Motor(BaseController):
             
         """
 
-        # d1 = tuple([np.abs(x) for x in d1])
-        # d2 = tuple([np.abs(x) for x in d2])
-        # d3 = tuple([np.abs(x) for x in d3])
-        # d4 = tuple([np.abs(x) for x in d4])
-
         self.d1 = np.sqrt(d1[0]**2+d1[1]**2)/self.u
         self.d2 = np.sqrt(d2[0]**2+d2[1]**2)/self.u
         self.d3 = np.sqrt(d3[0]**2+d3[1]**2)/self.u
         self.d4 = np.sqrt(d4[0]**2+d4[1]**2)/self.u
 
-        print(self.d1, self.d2, self.d3, self.d4)
+        print(f"{self.d1=}, {self.d2=}, {self.d3=}, {self.d4=}")
 
-        self.b1 = np.radians(np.tan(d1[0]/d1[1]))
-        self.b2 = np.radians(np.tan(d2[0]/d2[1]))
-        self.b3 = np.radians(np.tan(d3[0]/d3[1]))
-        self.b4 = np.radians(np.tan(d4[0]/d4[1]))
-
-        print(self.b1, self.b2, self.b3, self.b4)
 
     def set_r(self, r=33.5):
         """_summary_
@@ -184,6 +216,8 @@ class Motor(BaseController):
             r (float, radius): radius of wheels. Defaults to 33.5mm.
         """
         self.r = r/self.u
+
+        print(f"{self.r=}")
 
     @staticmethod
     def add_cls_specific_arguments(parent):
