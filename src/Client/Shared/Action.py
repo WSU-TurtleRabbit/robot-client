7bfd36931d17ffeb18c89165035e644fa6a7340a
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-01-30 19:46:50 +1100

    robot id is also sent with action

--- src/Client/Shared/Action.py
+++ src/Client/Shared/Action.py
@@ -1,19 +1,21 @@
 #! /usr/bin/env python3 -B
 
 class Action:
-    def __init__(self, vx: float, vy: float, omega: float, kick: int, dribble: float):
+    def __init__(self, id: int, vx: float, vy: float, vw: float, kick: int, dribble: float):
         """_summary_
             Object for initialise action commands, encode / decode strings for UDP transportation.
         Args:
+            id (int): client to control
             vx (float): wanted velocity for x direction
             vy (float): wanted velocity for y direction
-            omega (float): wanted angular velocity (radians)
+            vw (float): wanted angular velocity (radians)
             kick (bool): wanted kicker to kick (1=True,0=False)
             dribble (float): dribbling speed ? 
         """
+        self.id = int(id)
         self.vx = float(vx)
         self.vy = float(vy)
-        self.omega = float(omega)
+        self.vw = float(vw)
         self.kick = int(kick)
         self.dribble = float(dribble)
     
@@ -25,7 +27,7 @@ class Action:
         Returns:
             message(string): string for send message to send
         """
-        self.msg = f"{self.vx} {self.vy} {self.omega} {self.kick} {self.dribble}"
+        self.msg = f"{self.id} {self.vx} {self.vy} {self.vw} {self.kick} {self.dribble}"
         return self.msg
 
     @staticmethod
@@ -38,17 +40,17 @@ class Action:
         Returns:
             Action (Object): new Action object Model for easier attribute access
         """
-        vx, vy, omega, kick, dribble = msg.split(" ")
+        id, vx, vy, vw, kick, dribble = msg.split(" ")
         print(msg)
-        args = [float(vx), float(vy), float(omega), int(kick), float(dribble)]
+        args = [int(id), float(vx), float(vy), float(vw), int(kick), float(dribble)]
         return Action(*args)
     
     def __repr__(self): #debug msg
-        return f"Action: (vx: {self.vx}, vy: {self.vy}, theta: {self.omega}, kick: {self.kick}, dribble: {self.dribble})"
+        return f"Action: (id: {self.id}) (vx: {self.vx}, vy: {self.vy}, theta: {self.vw}, kick: {self.kick}, dribble: {self.dribble})"
     
 
 if __name__ == '__main__':
-    action = Action(1.0, 1.0, 1.0, True, 0.0)
+    action = Action(1, 1.0, 1.0, 1.0, True, 0.0)
     print(action)
     message = action.encode()
 
