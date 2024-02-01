96a14a2bf2ee3a9e6443ab48c98bc3bbe8bf1ca0
Emmama222 <20544166@student.westernsydney.edu.au>
2024-02-02 00:51:14 +1100

    using input()

--- Ardunio/sample.py
+++ Ardunio/sample.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     # access serial device at /dev/cu.usbmodem101 using baudrate of 192000
     with serial.Serial('/dev/cu.usbmodem101', 19200) as s: 
         # write a 'K'
-        s.write(b'K')
+        while True:
+            input()
+            s.write(b'K')
         # release the serial device to avoid a 'device is busy' error
         s.close()
