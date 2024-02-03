eb2390c792af6502b45e1c1ae502e93a8e24d440
Emmama222 <111248262+Emmama222@users.noreply.github.com>
2024-02-03 21:12:26 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,8 +26,8 @@ sudo apt-get update
 # pyenv global 3.7.12
 sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED*
 
-# libbcm='/lib/aarch64-linux-gnu/libbcm_host.so'
-# [[ -f $libbcm.0 ]] && sudo mv $libbcm.0 $libbcm
+libbcm='/lib/aarch64-linux-gnu/libbcm_host.so'
+[[ -f $libbcm.0 ]] && sudo mv $libbcm.0 $libbcm
 
 # pyenv uninstall -f 3.7.12
 
