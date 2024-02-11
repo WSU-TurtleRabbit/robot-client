ab3ae2ff1bb65374fae1e96d04cea28cc131536f
Emmama222 <7614175+jsnng@users.noreply.github.com>
2024-02-12 10:18:54 +1100

    added comments to decorators

--- src/Client/utils.py
+++ src/Client/utils.py
@@ -8,21 +8,40 @@ def redirect_print_to_log():
     def outer(func):
         @functools.wraps
         def inner(*args, **kwargs):
+            # make a copy of sys.stdout and sys.stderr
             stdout = sys.stdout
             stderr = sys.stderr
 
+            # set flag to make a new log file
             flag = "a"
 
+            # if the log file already exists, append log to it
             if os.path.exists(log):
                 flag = "w+"
 
+            # open up the log file for writing
+            # doesn't check if we have the permissions to write to it but
             with open(log, flag) as f:
+                # change sys.stdout and sys.stderr to log file
                 sys.stdout = f
                 sys.stderr = f
 
+                # run func
                 func(*args, **kwargs)
 
+            # reset sys.stdout and sys.stderr
             sys.stdout = stdout
             sys.stderr = stderr
         return inner
     return outer
+
+def runtime():
+    def outer(func: callable):
+        @functools.wraps
+        def inner(*args, **kwargs):
+            # record the start time
+            func(*args, **kwargs)
+            # record the end time
+            # calculate the runtime of the func
+        return
+    return outer 
