931ba3b516384bca0f18ae0a38b71cb3a049966a
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-02-13 08:03:00 +1100

    longer serial messages supported

--- Ardunio/Kicker/Kicker.ino
+++ Ardunio/Kicker/Kicker.ino
@@ -3,25 +3,37 @@ uint32_t previousTime = 0;
 uint32_t pulseTime = 1000; // pulseTime is in milliseconds
 bool pinStatus = LOW;
 
+const unsigned int MAX_MESSAGE_LEN = 9;
+
 bool checkPulsePin(bool, int, uint32_t, uint32_t, uint32_t) ; // function statement for compiling
 
 void setup() {
   Serial.begin(19200); // set baudrate to 19200
   pinMode(outputPin, OUTPUT); // set outputPin mode to output
-  while(Serial.available() != 1); // wait until serial communications is working 
 }
 
 void loop() {
-  char data;
+  char inByte;
   uint32_t currentTime = millis();
   pinStatus = checkPulsePin(pinStatus, outputPin, previousTime, currentTime, pulseTime);
-  data = Serial.read();
-  if(data == 'K') {
-    // check if `pinStatus` is still high from previous pulse
-    if(pinStatus == LOW) {
-      pinStatus = HIGH;
-      previousTime = currentTime;
+  while(Serial.available() > 0) { // check if there is something in the serial recv buffer to read
+    static char message[MAX_MESSAGE_LEN];
+    static unsigned int position = 0;
+    inByte = Serial.read(); // Serial.read() reads 1 byte at a time
+    if(inByte == '\n') { // check if the entire message has been recved
+      message[position] = '\0' // add a EOF character
+      break;
     }
+    // keep filling message buffer if not
+    message[position] = inByte; 
+    position++;
+  }
+    if(message == 'K') {
+      // check if `pinStatus` is still high from previous pulse
+      if(pinStatus == LOW) {
+        pinStatus = HIGH;
+        previousTime = currentTime;
+      }
   }
   digitalWrite(outputPin, pinStatus);
 }
