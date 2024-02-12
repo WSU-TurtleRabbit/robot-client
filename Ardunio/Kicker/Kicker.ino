90198fe750605dfb5e0011d43660ad787ec461f8
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-02-13 00:56:53 +1100

    non-blocking pulse

--- Ardunio/Kicker/Kicker.ino
+++ Ardunio/Kicker/Kicker.ino
@@ -1,32 +1,35 @@
 const int outputPin = 8;
-void pulsePin(int outputPin, int pulseTime); // function statement for compiling
+uint32_t previousTime = 0;
+uint32_t pulseTime = 1000; // pulseTime is in milliseconds
+bool pinStatus = LOW;
+
+bool checkPulsePin(bool, int, uint32_t, uint32_t, uint32_t) ; // function statement for compiling
 
 void setup() {
   Serial.begin(19200); // set baudrate to 19200
   pinMode(outputPin, OUTPUT); // set outputPin mode to output
+  while(Serial.available() != 1); // wait until serial communications is working 
 }
 
 void loop() {
   char data;
-  if(Serial.available() > 0) { 
-    data = Serial.read();
-    if(data == 'K') { // if the data recv'ed is 'K' (note: this is case sensitive)
-      pulsePin(outputPin, 20); 
+  uint32_t currentTime = millis();
+  pinStatus = checkPulsePin(pinStatus, outputPin, previousTime, currentTime, pulseTime);
+  data = Serial.read();
+  if(data == 'K') {
+    // check if `pinStatus` is still high from previous pulse
+    if(pinStatus == LOW) {
+      pinStatus = HIGH;
+      previousTime = currentTime;
     }
-    //TODO add dribble controls
   }
+  digitalWrite(outputPin, pinStatus);
 }
 
-void pulsePin(int outputPin, int pulseTime){
-  /*
-  * pulsePin(int outputPin, int pulseTime)
-  * sends a pulse (active high) into `outputPin` when called.
-  * args:
-  * outputPin (int): pin to send the pulse to
-  * pulseTime (int): length of pulse in microseconds
-  */
-  uint32_t currentTime = millis(); 
-  digitalWrite(outputPin, HIGH);
-  while(millis() < (currentTime + pulseTime));
-  digitalWrite(outputPin,LOW);
+bool checkPulsePin(bool pinStatus, int outputPin, uint32_t previousTime, uint32_t currentTime, uint32_t pulseTime) {
+  // if `previousTime` has been updated, check it has been longer than `pulseTime`
+  if(currentTime - previousTime >= pulseTime) {
+    pinStatus = LOW;
+  }
+  return pinStatus;
 }
