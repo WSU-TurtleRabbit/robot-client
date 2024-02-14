bb848dbd73f672f8e8ea37016b83bf2bf47ab932
Jason Ng <7614175+jsnng@users.noreply.github.com>
2024-02-14 17:43:35 +1100

    removed python 3.7 installation process

--- setup.sh
+++ setup.sh
@@ -3,22 +3,6 @@
 sudo apt-get update
 sudo apt-get upgrade --yes
 
-# [[ ! -d $HOME/.pyenv ]] && curl https://pyenv.run/ | bash
-
-# sudo apt-get install --yes libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev lzma lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev wget curl make build-essential openssl
-
-# touch ~/.bashrc
-
-# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-# echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-# echo 'eval "$(pyenv init --path)"' >> ~/.bashrc 
-# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc 
-# # exec $SHELL
-# source ~/.bashrc
-
-# pyenv update
-# pyenv install 3.7.12
-# pyenv global 3.7.12
 sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED*
 
 libbcm_host='/lib/aarch64-linux-gnu/libbcm_host.so'
