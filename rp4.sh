52c6dc41fc4ecde8ac1fefac9d436ed57a665bdb
Emma <20544166@student.westernsydney.edu.au>
2025-04-04 23:30:29 +1100

    added a shortcut for quick activation on virtual env. Also added additional comments

--- rp4.sh
+++ rp4.sh
@@ -5,7 +5,7 @@ set -e
 ### This script is for Raspberry Pi on Robot Only . ###
 ### Remember to do chmod 777 ./rp4.sh
 # Variables
-
+SHORT_CUT="activate-venv"
 VENV_NAME=".venv"
 RC_PATH="$HOME/robot-client"
 VENV_PATH="$RC_PATH/$VENV_NAME"
@@ -14,12 +14,13 @@ echo " - - - INITIALISING RP4 - - - "
 echo "Updating and Upgrading System" 
 sudo apt-get update
 sudo apt-get dist-upgrade --yes
+
 # ---- LEGACY CODE ----
 # sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED* # don't need this since working with virtual environments
-
-# libbcm_host='/lib/aarch64-linux-gnu/libbcm_host.so'
+# libbcm_host='/lib/aarch64-linux-gnu/libbcm_host.so' # can be fixed / replaced by libraspberrypi-dev
 # [[ -f $libbcm_host.0 ]] && sudo ln -s $libbcm_host.0 $libbcm_host
 # ---- LEGACY CODE -----
+
 echo "Locating Robot Client Repository"
 if [ ! -d "$RC_PATH" ]; then 
     cd $HOME
@@ -54,22 +55,45 @@ sudo apt install -y \
 pip install --upgrade pip
 pip install asyncqt importlib_metadata pyelftools
 pip install --no-deps moteus moteus_gui #moteus-pi3hat # comment out moteus-pi3hat when not on rp4
-sudo usermod -a -G dialout $USER
+sudo usermod -a -G dialout $USER #for Moteus-pi3hat only
 
 # ---- LEGACY CODE -----
 # MOTEUS_SITE_PACKAGES=$(python3 -c "import site; print([p for p in site.getsitepackages() if '.moteus-venv' in p][0])")
 # echo "$MOTEUS_SITE_PACKAGES"
 # export PYTHONPATH="$MOTEUS_SITE_PACKAGES:$PYTHONPATH"
 # ---- LEGACY CODE -----
+
 echo "Verifying possible import moteus in $VENV_PATH"
 # Verify that moteus is accessible
 python3 -c "import moteus; print('✅ Moteus is successfully linked!')"
 
 echo "Setup complete. You are now using $VENV_PATH with access to moteus!"
 
-# initialising this code module
-
+# initialising this code package as editable for development
 pip3 install --editable .
 
+## Doing a git pull
 git pull
 
+
+### ADDING DIRECT ACCESS TO ROBOT CLIENT VENV
+# Add a Convenient Alias (Optional)
+BASHRC_FILE="$HOME/.bashrc"
+if ! grep -q "alias $SHORT_CUT=" "$BASHRC_FILE"; then
+    echo "Creating ShortCut for accessing This Virtual Environment"
+
+    CMD="source '$VENV_PATH'/bin/activate"
+    echo "alias $SHORT_CUT=\"$CMD\"" >> "$BASHRC_FILE"
+    echo "New Command $SHORT_CUT has been added."
+    
+else 
+    echo "Command with $SHORT_CUT Already Exists, try using a different name or remove from $BASHRC_FILE"
+fi
+
+# Reload Bash Configuration
+echo "Reloading shell configuration..."
+source "$BASHRC_FILE"
+
+echo "You can now type '$SHORT_CUT' in terminal to activate this Virtual Environment"
+echo "To Deactivate, type 'deactivate' "
+echo "You have reached the end yay !- Emma :D"
