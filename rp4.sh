2698a5635b57a7b809ba7e29f121f077f8e6b2eb
Emma <20544166@student.westernsydney.edu.au>
2025-04-04 23:07:16 +1100

    combined moteus and robotclient virtual environment into 1 single environment. Tested on Ubuntu. *Need to test on Pi *This creates a single combined virtual env. including tview etc.

--- rp4.sh
+++ rp4.sh
@@ -1,69 +1,75 @@
-#! /bin/bash
+#!/usr/bin/env bash
+# Stop execution on any error
+set -e
 
 ### This script is for Raspberry Pi on Robot Only . ###
+### Remember to do chmod 777 ./rp4.sh
+# Variables
 
+VENV_NAME=".venv"
+RC_PATH="$HOME/robot-client"
+VENV_PATH="$RC_PATH/$VENV_NAME"
+
+echo " - - - INITIALISING RP4 - - - " 
+echo "Updating and Upgrading System" 
 sudo apt-get update
 sudo apt-get dist-upgrade --yes
+# ---- LEGACY CODE ----
+# sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED* # don't need this since working with virtual environments
 
-sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED*
-
-libbcm_host='/lib/aarch64-linux-gnu/libbcm_host.so'
-[[ -f $libbcm_host.0 ]] && sudo ln -s $libbcm_host.0 $libbcm_host
-
-# upgrade pip 
-sudo pip install --upgrade pip
-
-MOTEUS_PATH= "$HOME/moteus-venv"
-RC_PATH= "$HOME/robot-client"
-VENV_NAME= ".venv"
-VENV_PATH= "$RC_PATH/$VENV_NAME"
-
-# Step 1: Create the moteus virtual environment if it doesn't exist
-if [ ! -d "$MOTEUS_PATH" ]; then
-    echo "Installing Moteus Virtual environment and Modules"
-    sudo apt-get install --yes python3-pyside2* python3-serial python3-can python3-matplotlib python3-qtconsole
-    echo "Creating moteus-venv..."
-    python -m venv --system-site-packages "$MOTEUS_PATH"
-    source $MOTEUS_PATH/bin/activate
+# libbcm_host='/lib/aarch64-linux-gnu/libbcm_host.so'
+# [[ -f $libbcm_host.0 ]] && sudo ln -s $libbcm_host.0 $libbcm_host
+# ---- LEGACY CODE -----
+echo "Locating Robot Client Repository"
+if [ ! -d "$RC_PATH" ]; then 
+    cd $HOME
+    echo "Cloning Robot Client"
+    git clone "https://github.com/WSU-TurtleRabbit/robot-client.git"
 else
-    echo "moteus-venv already exists. Skipping creation."
-    source $MOTEUS_PATH/bin/activate
-fi
+    echo "Robot Client found @ $RC_PATH"
+fi 
+cd $RC_PATH
 
-echo "Checking for Packages for moteus"
-pip3 install moteus
-pip3 install asyncqt importlib_metadata pyelftools
-pip3 install moteus
-pip3 install moteus-pi3hat
-pip3 install --no-deps moteus_gui
-deactivate
-
-# Step 2: Create your own editable virtual environment
-if [ ! -d "$RC_PATH/.venv" ]; then
+## if you are using a system that requires python, you will have a message that warns you about externally managed environment
+## This means we need to either : break system package (do our modification) or build virtual environments, hence the following
+echo "Locating Robot Client Virtual Environment @ $VENV_PATH"
+if [ ! -d "$VENV_PATH" ]; then
     echo "Creating robot-client venv..."
-    python3 -m venv .venv
+    python3 -m venv --system-site-packages "$VENV_NAME"
 else
-    echo ".venv already exists. Skipping creation."
+    echo "$VENV_NAME already exists. Skipping creation."
 fi
+echo "Activating Virtual Environment $VENV_NAME @ $VENV_PATH . . ."
+source $VENV_PATH/bin/activate
 
-# Step 3: Find the site-packages directory of moteus-venv
-MOTEUS_SITE_PACKAGES=$(python3 -c "import site; print([p for p in site.getsitepackages() if 'moteus-venv' in p][0])")
+echo "Installing PIP Packages for moteus"
+sudo apt install -y \
+    python3-pyside2* \
+    python3-serial \
+    python3-can \
+    python3-matplotlib \
+    python3-qtconsole \
+    # libraspberrypi-dev # comment when not on raspberry pi 
 
-# Step 4: Add moteus-venv site-packages to PYTHONPATH
-echo "Linking moteus-venv to my-venv..."
-export PYTHONPATH="$MOTEUS_SITE_PACKAGES:$PYTHONPATH"
+pip install --upgrade pip
+pip install asyncqt importlib_metadata pyelftools
+pip install --no-deps moteus moteus_gui #moteus-pi3hat # comment out moteus-pi3hat when not on rp4
+sudo usermod -a -G dialout $USER
 
-# Step 5: Activate my-venv
-echo "Activating my-venv..."
-source .venv/bin/activate
-
-# Step 6: Verify that moteus is accessible
+# ---- LEGACY CODE -----
+# MOTEUS_SITE_PACKAGES=$(python3 -c "import site; print([p for p in site.getsitepackages() if '.moteus-venv' in p][0])")
+# echo "$MOTEUS_SITE_PACKAGES"
+# export PYTHONPATH="$MOTEUS_SITE_PACKAGES:$PYTHONPATH"
+# ---- LEGACY CODE -----
+echo "Verifying possible import moteus in $VENV_PATH"
+# Verify that moteus is accessible
 python3 -c "import moteus; print('✅ Moteus is successfully linked!')"
 
-echo "Setup complete. You are now using my-venv with access to moteus!"
+echo "Setup complete. You are now using $VENV_PATH with access to moteus!"
 
 # initialising this code module
 
-sudo pip3 install --editable .[prod] # installing Module as editable # add [prod] at the end if you are doing it on RP4
+pip3 install --editable .
 
 git pull
+
